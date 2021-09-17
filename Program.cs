ccb17b60d42f0c2fe80f4cf8e61190e9c13210f8
tdnphuc <18t1021242@husc.edu.vn>
2021-09-17 20:28:27 +0700

    Revert "Revert "helloword""
    This reverts commit f2e3218e5d4b65eb437267e8393c1ebd87999788.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lap_trinh_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
